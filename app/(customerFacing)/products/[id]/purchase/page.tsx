3930289475f0a9b561054632fc6f98b8911466e5
Nikhil Shakohi <nshakohi@apple.com>
2024-08-18 20:00:27 +0530

    feat(purchase): added payment handling ui and backend

---
+++ app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -0,0 +1,33 @@
+import db from "@/dev/dev";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+import { CheckoutForm } from "./_components/CheckoutForm";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+// Check product and show Payment Form
+export default async function PurchasePage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await db.product.findUnique({ where: { id } });
+  if (product == null) return notFound();
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.price,
+    currency: "INR",
+    metadata: { productId: product.id },
+  });
+
+  if (paymentIntent.client_secret == null) {
+    throw Error("Stripe failed to create payment intent");
+  }
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  );
+}
